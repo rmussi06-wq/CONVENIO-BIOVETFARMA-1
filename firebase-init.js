c8ee78b0feef45519ec37083bb23a6f8e6f8bc23
rmussi06-wq <rmussi06@gmail.com>
2025-10-24 18:22:55 -0300

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,15 +1,1 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyBwQzMILAfxVqxnB4_ZNMfAZ1hTOigI-o8",
-  authDomain: "conveniobvf.firebaseapp.com",
-  projectId: "conveniobvf",
-  storageBucket: "conveniobvf.firebasestorage.app",
-  messagingSenderId: "783050233537",
-  appId: "1:783050233537:web:6719d99f4cff28f03a6640",
-  measurementId: "G-SVEJKDM715"
-};
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+// placeholder para Firebase
